8283d5717c6bb147ba4a4e4882826e1f7125313c
Laye 'Leo' Mehta <layeamehta@gmail.com>
2022-04-04 12:19:28 -0400

    added post and get route

--- server/index.js
+++ server/index.js
@@ -6,6 +6,30 @@ const app = express();
 app.use(cors()); 
 app.use(express.json()); 
 
+// CRUD routes
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body; 
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *", 
+      [description]
+    ); 
+  } catch (err) {
+    console.error(err.message);     
+  }
+}); 
+
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo"); 
+    res.json(allTodos.rows); 
+    
+  } catch (err) {
+    console.log(err.message); 
+    
+  }
+}); 
 
 app.listen(5000, () => {
     console.log("server has started on port 5000");
